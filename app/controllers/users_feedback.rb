a834d3be0d1e6291d1e61e26a984bd810bae34ec
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-02-13 10:51:21 -0500

    setting up feedback controller

--- app/controllers/users_feedback.rb
+++ app/controllers/users_feedback.rb
@@ -1,2 +1,20 @@
 class UsersFeedback < ApplicationController
+  before_action :set_user_feedback!, only: [:show, :edit, :update, :destroy]
+  before_action :authorized
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+
+  private
+
+  def set_user_feedback!
+    @user_feedback = UserFeedback.find(params[:id])
+  end
 end
