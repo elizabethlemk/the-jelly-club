248fa322ec1eeac699b97774aee5c710ccad0c4d
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-02-15 10:46:47 -0500

    fixed like issue with other users. Added to find_like method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,19 +3,21 @@ class LikesController < ApplicationController
   before_action :find_like, only: [:destroy]
 
  def create
+   byebug
    @sandwich.likes.create(user_id: current_user.id)
    redirect_to sandwich_path(@sandwich)
  end
 
  def destroy
+   byebug
    @like.destroy
    redirect_to sandwich_path(@sandwich)
  end
 
  def like_destroy_all
-   current_user.likes.each do |likes|
-     if likes != nil
-       likes.destroy
+   current_user.likes.each do |like|
+     if like != nil
+       like.destroy
      end
    end
    redirect_to users_path
@@ -30,6 +32,6 @@ class LikesController < ApplicationController
  end
 
  def find_like
-   @like = @sandwich.likes.find_by(sandwich_id: params[:sandwich_id])
+   @like = @sandwich.likes.find_by(sandwich_id: params[:sandwich_id], user_id: current_user.id)
 end
 end
